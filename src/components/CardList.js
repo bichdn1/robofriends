bebfbd4a2458419e436f489e90227dc500da1ef6
bichdn1 <duongbich.dev94@gmail.com>
2021-06-06 16:46:13 +0700

    update web page

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-  if (true) {
-    throw new Error('Nooooo');
-  }
+  // if (true) {
+  //   throw new Error('Nooooo');
+  // }
   return (
     <>
       {!!robots.length && robots.map(robot => (<Card key={robot.id} robot={robot}/>))}
